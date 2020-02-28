eebf5196dce8ea47b1571649f6d663eba242bd60
Nico Loparco <nicoloparco75@gmail.com>
2020-02-27 21:48:09 -0500

    completed layout and structure to begin app

---
+++ seeders/seeds.sql
@@ -0,0 +1,4 @@
+USE heroku_c929ea8326567c4;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Hamburger", false), ("Cheeseburger", false), ("Bacon Burger", false), ("Royale with Cheese", false)
--- seeds.sql
+++
@@ -1,4 +0,0 @@
-USE heroku_4affa8582a960a4;
-
-INSERT INTO burgers (burger_name, devoured)
-VALUES ("Hamburger", false), ("Cheeseburger", false), ("Bacon Burger", false), ("Royale with Cheese", false)
