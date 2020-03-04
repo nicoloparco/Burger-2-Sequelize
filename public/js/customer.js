355ef48796896b90e867a1fd773391aa179930aa
Nico Loparco <nicoloparco75@gmail.com>
2020-03-04 18:57:50 -0500

    added js and jquery for customer

---
+++ public/js/customer.js
@@ -0,0 +1,75 @@
+$(document).ready(function() {
+    var customerName = $("#customerName");
+    var customerList = $("#customerList");
+    var customerContainer = $(".customerContainer")
+    
+    $(document).on("submit", "customerForm", handleFormSubmit)
+    $(document).on("click", "deleteCustomer", handleDeleteButtonPress)
+
+    getCustomers();
+
+    function handleFormSubmit(event){
+        event.preventDefault();
+        
+        if(!customerName.val().trim()){
+            return;
+        } else {
+            addCustomer({
+                name: customerName.val().trim()
+            });
+        }
+    }
+
+    function addCustomer(customer) {
+        $.post("/api/customers", customer).then(getCustomers)
+    }
+
+    function createCustomerRow(customer) {
+        var newTr = $("<tr");
+        newTr.data("customer", customer);
+        newTr.append("<td>" + customer.name + "</td>");
+        if(customer.Burgers) {
+            newTr.append("<td>" + customer.Burgers.length + "</td>")
+        } else {
+            newTr.append("<td>0</td>")
+        }
+        newTr.append("<td><a class='delete-author'>Delete Author</a.</td>")
+    }
+
+    function getCustomers() {
+        $.get("api/customers", function(data) {
+            var rowsToAdd = [];
+            for(var i = 0; i < data.length; i++) {
+                rowsToAdd.push(createCustomerRow(data[i]));
+            }
+            renderCustomerList(rowsToAdd);
+            customerName.val("")
+        })
+    }
+
+    function renderCustomerList() {
+        customerList.children().not(":last").remove();
+        customerContainer.children(".alert").remove()
+        if(rows.length) {
+            customerList.prepend(rows)
+        } else {
+            renderEmpty();
+        }
+    }
+
+    function renderEmpty() {
+        var alertDiv = $("<div>");
+        alertDiv.addClass("alert");
+        alertDiv.text("Customer must enter name before accessing Burgers.");
+        authorContainer.append(alertDiv);
+    }
+
+    function handleDeleteButtonPress() {
+        var listItemData = $(this).parent("td").parent("tr").data("customer");
+        var id = listItemData.id;
+        $.ajax({
+            method: "DELETE",
+            url: "/api/customers/" + id
+        }).then(getCustomers);
+    }
+});
