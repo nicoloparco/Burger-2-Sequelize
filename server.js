cbdfb55d1ef3baa8d482a7e5d2c95181e595f991
Nico Loparco <nicoloparco75@gmail.com>
2020-03-04 17:43:49 -0500

    initialized tailwindcss

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const exphbs = require("express-handlebars")
 const app = express();
 const PORT = process.env.PORT || 8080;
 const db = require("./models");
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(express.static.apply("public"));
+app.use(express.static(__dirname + "./public"));
+
+app.engine("handlebars", exphbs({defaultLayout: "main"}))
+app.set("view engine", "handlebars")
 
 require("./routes/burger-api-routes.js")(app);
 require("./routes/customer-api-routes.js")(app);
