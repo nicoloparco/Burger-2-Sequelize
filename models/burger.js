1d458ceb04bf910064892675dd5ae28d727661a0
Nico Loparco <nicoloparco75@gmail.com>
2020-03-03 22:07:24 -0500

    updated burger model

--- models/burger.js
+++ models/burger.js
@@ -2,14 +2,27 @@ module.exports = function(sequelize, DataTypes) {
     var Burger = sequelize.define("Burger", {
         name: {
             type: DataTypes.STRING,
-            allowNull: false
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
         },
         devoured: {
             type: DataTypes.BOOLEAN,
             defaultValue: false
         }
     });
+
+    Burger.associate = function(models) {
+        Burger.belongsTo(models.Customer, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+    };
+
+
     return Burger;
-}
+};
 
 
