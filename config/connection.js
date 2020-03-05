8e935c4a929b1818c5762f22b41daade90170ac4
Nico Loparco <nicoloparco75@gmail.com>
2020-03-04 19:09:43 -0500

    testing connection

--- config/connection.js
+++ config/connection.js
@@ -12,10 +12,10 @@ var sequelize = new Sequelize ("heroku_c929ea8326567c4", "b4f46f9fddd68a", "12e9
 
 module.exports = sequelize;
 
-var connection = mysql.createConnection({
-    host: "us-cdbr-iron-east-05.cleardb.net",
-    port: 3306,
-    user: "bff44ce58e9705",
-    password: "676e3259",
-    database: "heroku_4affa8582a960a4"
-});
+// var connection = mysql.createConnection({
+//     host: "us-cdbr-iron-east-05.cleardb.net",
+//     port: 3306,
+//     user: "bff44ce58e9705",
+//     password: "676e3259",
+//     database: "heroku_4affa8582a960a4"
+// });
