0f65a4d257c16de37cb6cf38b4b0435bc8c49096
Nico Loparco <nicoloparco75@gmail.com>
2020-03-05 20:49:45 -0500

    config folder adjustement

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const Sequelize  = require("sequelize");
-var sequelize = new Sequelize ("sequelize_burgers", "root", "docker", {
-    host: "localhost",
+var sequelize = new Sequelize ("heroku_c929ea8326567c4", "b4f46f9fddd68a", "12e9811b", {
+    host: "us-cdbr-iron-east-04.cleardb.net",
     port: 3306,
     dialect: "mysql",
     pool: {
